cefdf57756ea0059646d07bc085ddd0f721a2f2a
ynrrizki <78051294+ynrrizki@users.noreply.github.com>
2023-05-10 18:53:26 +0700

    static tabbar height

--- lib/ui/widgets/detail_tabbar_widget.dart
+++ lib/ui/widgets/detail_tabbar_widget.dart
@@ -84,3 +84,69 @@ class _DetailTabBarWidgetState extends State<DetailTabBarWidget>
     );
   }
 }
+
+
+
+// import 'package:flutter/material.dart';
+
+// class DetailTabBarWidget extends StatefulWidget {
+//   const DetailTabBarWidget({
+//     super.key,
+//   });
+
+//   @override
+//   State<DetailTabBarWidget> createState() => _DetailTabBarWidgetState();
+// }
+
+// class _DetailTabBarWidgetState extends State<DetailTabBarWidget>
+//     with SingleTickerProviderStateMixin {
+//   late TabController _tabController;
+
+//   List<String> _tabs = ['Tab 1', 'Tab 2'];
+//   List<Widget> _tabViews = [
+//     Container(
+//       child: Center(
+//         child: Text('Tab 1'),
+//       ),
+//     ),
+//     Container(
+//       child: Center(
+//         child: Text('Tab 2'),
+//       ),
+//     ),
+//   ];
+
+//   @override
+//   void initState() {
+//     _tabController = TabController(length: 2, vsync: this);
+//     super.initState();
+//   }
+
+//   @override
+//   void dispose() {
+//     _tabController.dispose();
+//     super.dispose();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     // Create a custom TabBar. Don't use BuiltIn Tab bar
+//     return Column(
+//       children: [
+//         TabBar(
+//           controller: _tabController,
+//           tabs: _tabs.map((e) => Tab(text: e)).toList(),
+//         ),
+//         ListView(
+//           shrinkWrap: true,
+//           children: [
+//             TabBarView(
+//               controller: _tabController,
+//               children: _tabViews.map((e) => e).toList(),
+//             ),
+//           ],
+//         ),
+//       ],
+//     );
+//   }
+// }
