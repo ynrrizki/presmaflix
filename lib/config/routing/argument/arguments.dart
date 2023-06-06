ecedb4b16985c73b518c85d968783ba456eeef9f
ynrrizki <78051294+ynrrizki@users.noreply.github.com>
2023-06-07 01:53:39 +0700

    base import from all arguments

---
+++ lib/config/routing/argument/arguments.dart
@@ -0,0 +1,10 @@
+/*
+  This file is used to import all arguments in this project
+  so that it can be accessed from one file only
+  and can be used in other files
+*/
+
+// Path: lib\config\routing\argument\content
+export 'content/content_all_args.dart';
+export 'content/content_detail_args.dart';
+export 'content/content_video_args.dart';
