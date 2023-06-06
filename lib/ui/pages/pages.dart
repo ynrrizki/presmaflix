36da9d8882f81e2556589c8a8e4a8c45aef35e82
ynrrizki <78051294+ynrrizki@users.noreply.github.com>
2023-06-07 01:51:04 +0700

    add: base import from all pages

---
+++ lib/ui/pages/pages.dart
@@ -0,0 +1,20 @@
+/*
+  This file is used to import all pages in this project
+  so that it can be accessed from one file only
+  and can be used in other files
+*/
+// Path: lib\ui\auth
+export 'auth/login_page.dart';
+// Path: lib\ui\menu
+export 'menu/bottom_navigation.dart';
+// - home
+export 'menu/home/home_page.dart';
+export 'menu/home/content/content_all_page.dart';
+export 'menu/home/content/content_detail_page.dart';
+export 'menu/home/content/content_video_page.dart';
+// - search
+export 'menu/search/search_page.dart';
+// - watchlist
+export 'menu/watchlist/watchlist_page.dart';
+// - more
+export 'menu/more/more_page.dart';
