b814dca25cd9cdd24f913072ef5301be52176976
ynrrizki <78051294+ynrrizki@users.noreply.github.com>
2023-06-07 02:00:31 +0700

    base import from all widgets

---
+++ lib/ui/widgets/widgets.dart
@@ -0,0 +1,15 @@
+/*
+  This file is used to import all widgets in this project
+  so that it can be accessed from one file only
+  and can be used in other files
+*/
+
+export 'banner_widget.dart';
+export 'card_video_widget.dart';
+export 'horizontal_grid_widget.dart';
+export 'poster_widget.dart';
+export 'search_field_widget.dart';
+export 'skleton_horizontal_grid_widget.dart';
+export 'tabbar_controller.dart';
+export 'tabbar_widget.dart';
+export 'text_field_widget.dart';
