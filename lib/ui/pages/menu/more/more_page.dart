6843772d165c9e0148307a527e908ea20fafe204
ynrrizki <78051294+ynrrizki@users.noreply.github.com>
2023-06-08 18:08:30 +0700

    add: creating ui in more_page

--- lib/ui/pages/menu/more/more_page.dart
+++ lib/ui/pages/menu/more/more_page.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -26,24 +27,59 @@ class MorePage extends StatelessWidget {
         children: [
           Padding(
             padding: const EdgeInsets.all(8.0),
-            child: SizedBox(
-              height: 100,
-              child: Row(
-                children: const [],
+            child: Card(
+              color: Theme.of(context).scaffoldBackgroundColor,
+              borderOnForeground: false,
+              child: ListTile(
+                contentPadding:
+                    const EdgeInsets.symmetric(horizontal: 0, vertical: 10),
+                onTap: () {},
+                leading: const CircleAvatar(
+                  radius: 40,
+                  backgroundImage: CachedNetworkImageProvider(
+                    'https://ui-avatars.com/api/?name=Yanuar Rizki Sanjaya',
+                  ),
+                ),
+                title: const Text('Yanuar Rizki Sanjaya'),
+                subtitle: const Text('yanuarrizki165@gmail.com'),
               ),
             ),
           ),
-          Container(
-            decoration: BoxDecoration(
-              color: Theme.of(context).bottomNavigationBarTheme.backgroundColor,
-              boxShadow: const [
-                BoxShadow(
-                  color: Colors.grey,
-                  blurRadius: 0.5,
-                ),
-              ],
+          Card(
+            color: Colors.black,
+            shadowColor: Colors.grey,
+            elevation: 0.5,
+            child: ListTile(
+              leading: const Icon(Icons.search_outlined),
+              title: const Text('Search'),
+              trailing: const Icon(Icons.arrow_forward_ios),
+              onTap: () => Navigator.of(
+                context,
+                rootNavigator: true,
+              ).pushNamed('/search'),
             ),
-            height: 100,
+          ),
+          Card(
+            color: Colors.black,
+            shadowColor: Colors.grey,
+            elevation: 0.5,
+            child: ListTile(
+              leading: const Icon(Icons.notifications_active_outlined),
+              title: const Text('Notifications'),
+              trailing: const Icon(Icons.arrow_forward_ios),
+              onTap: () {},
+            ),
+          ),
+          Card(
+            color: Colors.black,
+            shadowColor: Colors.grey,
+            elevation: 0.5,
+            child: ListTile(
+              leading: const Icon(Icons.settings_outlined),
+              title: const Text('Settings'),
+              trailing: const Icon(Icons.arrow_forward_ios),
+              onTap: () {},
+            ),
           ),
         ],
       ),
